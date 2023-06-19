7986170af9cde9c89dd3b402f26fbb08c49fd8f5
codertanish <81834790+codertanish@users.noreply.github.com>
2023-06-19 18:07:34 -0400

    Add files via upload

--- FibonacciSum.py
+++ FibonacciSum.py
@@ -6,22 +6,26 @@ root.configure(bg = "red1")
 
 number_of_digits = Entry(root)
 digits = number_of_digits.get()
-series = Label(root, font = ("Comic Sans MS", 12, "bold"), text = "The First " + str(digits) + "Terms Of The Fibonacci Series Are ")
+series = Label(root, font = ("Comic Sans MS", 12, "bold"), text = "The First " + str(digits) + " Terms Of The Fibonacci Series Are ")
 fibonacci_sum = Label(root, font = ("Comic Sans MS", 12, "bold"))
 def Fibonacci():
-    num = int(number_of_digits.get())
+    fibonacci = [0,1]
+    num = int(number_of_digits.get())-2
     n1 = 0
     n2 = 1
     sum = 0
-    i = 1
+    i = 0
     total_sum = n1 + n2
-    while(i<=num):
-        series["text"] += str(sum) + ", "
+    while(i<num):
         sum = n1 + n2
         n1 = n2
         n2 = sum
+        fibonacci.append(sum)
         total_sum += n2
-        fibonacci_sum["text"] = "The Sum Of The First " + str(num) + "Terms Of The Fibonacci Series Is " + str(total_sum)
+        i += 1
+        
+    series["text"] = "The First " + str(number_of_digits.get()) + " Terms Of The Fibonacci Series Are " + str(fibonacci)
+    fibonacci_sum["text"] = "The Sum Of The First " + str(number_of_digits.get()) + " Terms Of The Fibonacci Series Is " + str(total_sum)
         
 show_button = Button(root, command = Fibonacci, text = "Show Fibonacci Series and Sum", fg = "black", font = ("Comic Sans MS", 12, "bold"), bg = "red2")
   
